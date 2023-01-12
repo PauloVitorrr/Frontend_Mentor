fb9f321a9fdcf5d3eaa683d25c9400ccd03a428c
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-01-12 10:06:52 -0300

    iniciando validação

---
+++ intro-component-with-signup-form-master/js/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#form")
+const firstName = document.querySelector("#first-name")
+const lastName = document.querySelector("#lastName")
+const email = document.querySelector("#email")
+
+console.log(form, firstName)
