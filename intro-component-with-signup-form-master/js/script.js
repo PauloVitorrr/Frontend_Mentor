46d5fe8fb8fe6271b33bd60f9af7e4542cdd0948
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-21 00:33:56 -0300

    feat: não enviar o formulario com campo vazios

--- intro-component-with-signup-form-master/js/script.js
+++ intro-component-with-signup-form-master/js/script.js
@@ -2,5 +2,32 @@ const form = document.querySelector("#form")
 const firstName = document.querySelector("#first-name")
 const lastName = document.querySelector("#lastName")
 const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const span = document.querySelectorAll(".span-erro")
 
-console.log(form, firstName)
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+//     const teste = []
+//     span.forEach(function(i){
+//     const teste = [i]
+//     console.log(teste)
+// })
+
+    console.log(span)
+
+    if(firstName.value == ''){
+        span.classList.remove('span-erro')
+        span.classList.add('span-active')
+    } else if(lastName.value == ''){
+        span.classList.remove('span-erro')
+        span.classList.add('span-active')
+    } else if(email.value == ''){
+        span.classList.remove('span-erro')
+        span.classList.add('span-active')
+    } else if(password.value == ''){
+        span.classList.remove('span-erro')
+        span.classList.add('span-active')
+    }
+})
