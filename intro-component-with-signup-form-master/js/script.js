e7f0455d1cf10d4b33cd47a00d905fee6409d2a2
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-21 23:25:15 -0300

    feat: validação com form

--- intro-component-with-signup-form-master/js/script.js
+++ intro-component-with-signup-form-master/js/script.js
@@ -1,33 +1,43 @@
 const form = document.querySelector("#form")
 const firstName = document.querySelector("#first-name")
-const lastName = document.querySelector("#lastName")
+const lastName = document.querySelector("#last-name")
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
-const span = document.querySelectorAll(".span-erro")
 
+const span_name = document.querySelector(".span-name")
+const span_last = document.querySelector(".span-last")
+const span_email = document.querySelector(".span-email")
+const span_password = document.querySelector(".span-password")
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
-
-//     const teste = []
-//     span.forEach(function(i){
-//     const teste = [i]
-//     console.log(teste)
-// })
-
-    console.log(span)
-
+    
     if(firstName.value == ''){
-        span.classList.remove('span-erro')
-        span.classList.add('span-active')
-    } else if(lastName.value == ''){
-        span.classList.remove('span-erro')
-        span.classList.add('span-active')
-    } else if(email.value == ''){
-        span.classList.remove('span-erro')
-        span.classList.add('span-active')
-    } else if(password.value == ''){
-        span.classList.remove('span-erro')
-        span.classList.add('span-active')
+        firstName.classList.add('input-active')
+        span_name.classList.add('span-active')
+    } else {
+        firstName.classList.remove('input-active')
+        span_name.classList.remove('span-active')
+    }
+    if(lastName.value == ''){
+        lastName.classList.add('input-active')
+        span_last.classList.add('span-active')
+    } else {
+        lastName.classList.remove('input-active')
+        span_last.classList.remove('span-active')
+    }
+    if(email.value == ''){
+        email.classList.add('input-active')
+        span_email.classList.add('span-active')
+    } else {
+        span_email.classList.remove('span-active')
+        email.classList.remove('input-active')
+    } 
+    if(password.value == ''){
+        password.classList.add('input-active')
+        span_password.classList.add('span-active')
+    } else {
+        span_password.classList.remove('span-active')
+        password.classList.remove('input-active')
     }
 })
